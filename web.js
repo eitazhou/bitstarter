cb28ee8a2cdcbbc95413c181876780b8ba909394
Ubuntu <ubuntu@ip-10-156-255-25.ap-northeast-1.compute.internal>
2013-07-17 16:39:47 +0000

    try again

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 
     var fs = require('fs');
     var buffer = new Buffer(256);
-    var data = fs.readFileSync('./index.html');     
+    var data = fs.readFileSync('index.html');     
     response.send(buffer.toString("utf8", data)));
 });
 
