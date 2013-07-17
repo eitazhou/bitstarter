3f6354a5d0eddb5deadf4a2d2d17a90ab83459bc
Ubuntu <ubuntu@ip-10-156-255-25.ap-northeast-1.compute.internal>
2013-07-17 09:59:48 +0000

    modify web.js to show index.html

--- web.js
+++ web.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 
-var buffer=fs.readFileSync('./index.html');
+//var buffer=fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+
+//  response.send(buffer.toString());
+    var fs = require('fs');
+    var buffer = new buffer();
+    response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
+
+
 });
 
 var port = process.env.PORT || 5000;
